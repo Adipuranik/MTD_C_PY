30d954964481e63b6d3dad71210ae79e7f27a13c
Adipuranik <adipuranik1997@gmail.com>
2025-03-06 14:32:29 +0530

    p2.c program added

---
+++ day1/p1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int inutNum = 0;
+    puts("Enter a number to check if it is even"); //put string
+    scanf("%d",& inutNum);
+     if (inutnun % 2 == 0)
+      printf("%d is even", inutnum);
+    else 
+      printf("%d is odd", in)
+}
---
+++ day1/p2.c
@@ -0,0 +1,9 @@
+include<stdio.h>
+int main()
+{
+    int num1 =0;
+    long int num2 = 0;
+    long long int num3 = 0;
+    double d = 0.0;
+    float f = 0.0f;
+    printf("%d %d %d %d\n ,", size)
